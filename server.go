dca159bf5d75f2a84267625fa6d29782ad66d616
Kseniya Bialkevich <kseniyabialkevich@gmail.com>
2020-08-07 00:20:01 +0300

    [dev] added edit Handlers

--- server.go
+++ server.go
@@ -220,6 +220,133 @@ func CreateAnswerHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func EditVotingHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "GET" {
+
+		vars := mux.Vars(r)
+		id_voting := vars["id_voting"]
+
+		row := database.QueryRow("SELECT * FROM votingdb.votings WHERE id = ?", id_voting)
+
+		voting := Voting{}
+
+		err := row.Scan(&voting.ID, &voting.Name, &voting.Description, &voting.StartTime, &voting.EndTime)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, http.StatusText(404), http.StatusNotFound)
+		} else {
+			tmpl, _ := template.ParseFiles("templates/edit_voting.html")
+			tmpl.Execute(w, voting)
+		}
+	}
+
+	if r.Method == "POST" {
+
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+
+		id_voting := r.FormValue("id_voting")
+		name := r.FormValue("name")
+		description := r.FormValue("description")
+		startTime := r.FormValue("start_time")
+		endTime := r.FormValue("end_time")
+
+		_, err = database.Exec("UPDATE votingdb.votings set name = ?, description = ?, start_time = ?, end_time = ? WHERE id = ?", name, description, startTime, endTime, id_voting)
+		if err != nil {
+			log.Println(err)
+		}
+
+		http.Redirect(w, r, "/voting_qa/"+id_voting, 301)
+	}
+}
+
+func EditQuestionHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "GET" {
+
+		vars := mux.Vars(r)
+		id_question := vars["id_question"]
+
+		row := database.QueryRow("SELECT * FROM votingdb.questions WHERE id = ?", id_question)
+
+		question := Question{}
+
+		err := row.Scan(&question.ID, &question.Name, &question.ID_Voting)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, http.StatusText(404), http.StatusNotFound)
+		} else {
+			tmpl, _ := template.ParseFiles("templates/edit_question.html")
+			tmpl.Execute(w, question)
+		}
+	}
+
+	if r.Method == "POST" {
+
+		vars := mux.Vars(r)
+		id_voting := vars["id_voting"]
+
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+
+		id_question := r.FormValue("id_question")
+		name := r.FormValue("name")
+
+		_, err = database.Exec("UPDATE votingdb.questions set name = ? WHERE id = ?", name, id_question)
+		if err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/voting_qa/"+id_voting, 301)
+	}
+}
+
+func EditAnswerHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "GET" {
+
+		vars := mux.Vars(r)
+		id_answer := vars["id_answer"]
+
+		row := database.QueryRow("SELECT * FROM votingdb.answers WHERE id = ?", id_answer)
+
+		answer := Answer{}
+
+		err := row.Scan(&answer.ID, &answer.Name, &answer.ID_Question)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, http.StatusText(404), http.StatusNotFound)
+		} else {
+			tmpl, _ := template.ParseFiles("templates/edit_answer.html")
+			tmpl.Execute(w, answer)
+		}
+	}
+
+	if r.Method == "POST" {
+
+		vars := mux.Vars(r)
+		id_voting := vars["id_voting"]
+
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+
+		id_answer := r.FormValue("id_answer")
+		name := r.FormValue("name")
+
+		_, err = database.Exec("UDATE votingdb.answers set name = ? WHERE id = ?", name, id_answer)
+		if err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/voting_qa/"+id_voting, 301)
+	}
+}
+
 func main() {
 
 	db, err := sql.Open("mysql", "root:11111111@tcp(localhost:3306)/votingdb")
@@ -237,6 +364,9 @@ func main() {
 	router.HandleFunc("/voting_qa/{id_voting:[0-9]+}", VotingQAHandler)
 	router.HandleFunc("/create_question/{id_voting:[0-9]+}", CreateQuestionHandler)
 	router.HandleFunc("/create_answer/{id_voting:[0-9]+}/{id_question:[0-9]+}", CreateAnswerHandler)
+	router.HandleFunc("/edit_voting/{id_voting:[0-9]+}", EditVotingHandler)
+	router.HandleFunc("/edit_question/{id_voting:[0-9]+}/{id_question:[0-9]+}", EditQuestionHandler)
+	router.HandleFunc("/edit_answer/{id_voting:[0-9]+}/{id_answer:[0-9]+}", EditAnswerHandler)
 
 	http.Handle("/", router)
 
